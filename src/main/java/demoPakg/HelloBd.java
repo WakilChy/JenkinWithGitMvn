26cec64ffe17a14b1472a7fcdf1ec3f9d7218ab9
Wakil <wec070@msn.com>
2021-03-25 18:22:37 -0400

    V.4 updated from git Brash

--- src/main/java/demoPakg/HelloBd.java
+++ src/main/java/demoPakg/HelloBd.java
@@ -7,6 +7,8 @@ public class HelloBd {
 		System.out.println("Hello this is demo test class");
 		System.out.println("Hello this is demo test class V.2");
 		System.out.println("Hello this is demo test class V.3");
+		
+		System.out.println("Hello this is demo test class V.4");
 
 	}
 
