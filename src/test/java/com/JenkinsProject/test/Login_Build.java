2280f548ab3dc4ee8e31dc3cd68031e4cde7ebd0
Wakil <wec070@msn.com>
2021-03-25 11:28:54 -0400

    Jenkins & git collaboration

---
+++ src/test/java/com/JenkinsProject/test/Login_Build.java
@@ -0,0 +1,51 @@
+package com.JenkinsProject.test;
+
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login_Build {
+	
+	
+	@Test
+	public void CLead() {
+		//System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver 85.0.4183.87\\chromedriver.exe");
+		
+		WebDriverManager.chromiumdriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		
+		//Launch the browser 
+		driver.get("http://www.leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+		
+		
+		
+		//Input UserName & password
+		driver.findElement(By.xpath("//input[@id=\"username\"]")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		
+		
+		//Click login 
+		driver.findElement(By.xpath("//input[@type=\"submit\"]")).click();
+		
+		//Click CRM/SFA
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		
+		//Create Lead
+				driver.findElement(By.linkText("Create Lead")).click();
+				
+				
+				System.out.println("You are on JenkinsDemo package > Buile ");
+		
+		driver.close();
+		
+
+	}
+
+}
