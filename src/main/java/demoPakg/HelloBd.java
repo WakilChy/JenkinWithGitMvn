1828117cedbfc72d4e0debe01a5536a51d49a3e3
WAKIL CHOUDHURY <62894975+WakilChy@users.noreply.github.com>
2021-03-25 17:01:42 -0400

    Update HelloBd.java
    Demo class V.2 add

--- src/main/java/demoPakg/HelloBd.java
+++ src/main/java/demoPakg/HelloBd.java
@@ -5,6 +5,7 @@ public class HelloBd {
 	public static void main(String[] args) {
 
 		System.out.println("Hello this is demo test class");
+		System.out.println("Hello this is demo test class V.2");
 
 	}
 
