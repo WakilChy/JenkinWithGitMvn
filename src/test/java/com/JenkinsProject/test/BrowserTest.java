f6c6137424ad73e7e4c86bb8fa2d99c06f515e23
Wakil <wec070@msn.com>
2021-04-12 09:47:14 -0400

    BrowserTest file added

---
+++ src/test/java/com/JenkinsProject/test/BrowserTest.java
@@ -0,0 +1,35 @@
+package com.JenkinsProject.test;
+
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserTest {
+	
+	
+	@Test
+	public void CLead() {
+		//System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver 85.0.4183.87\\chromedriver.exe");
+		
+		WebDriverManager.chromiumdriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		
+		//Launch the browser 
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+		
+						
+		System.out.println("You are on Google Home page ");
+		
+		driver.close();
+		
+
+	}
+
+}
